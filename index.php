54dc7ba02e643fd3c9e21564e7e4882da5fb87be
Dan Dilag <root@vclops.dxc.com>
2017-12-04 08:38:31 -0500

    modify index

--- index.php
+++ index.php
@@ -3,14 +3,24 @@ include ("dbconnect.php");
          if(! $link ){
             die('Could not connect: ' . mysql_error());
          }
-         echo 'Connected successfully<br />'
 ?>
 
 <html>
-<head><title>Member Login</title></head>
+<head>
+<title>Member Login</title>
+<style>
+div {
+    background-color: black;
+}
+</style>
+</head>
 <body>
+<div>
+<p>
+<img src="dxc_logo_hz_wht_rgb_150.png" alt="DXC Logo" width="400" height="100">
+</p>
+</div>
 <center>
-<img src = "/dxc_logo_hz_wht_rgb_150.png" ></img>
 <H1>RTPA Portal Login</H1>
 <form name="LoginForm" action="../trans/verify.php" method="post">
 <p><b>Username:<input name="user" size="25" type="text" /></b></p>
@@ -21,13 +31,5 @@ include ("dbconnect.php");
 </select></p>
 <p><input name="submit" type="submit" value="Submit" /></p>
 </form>
-<iframe
-name="iframe"
-width="70%"
-height="40%"
-src="http://www.dxc.technology/"
-frameborder="no"
-scrolling="auto">
-</iframe>
 </body>
 </html>
