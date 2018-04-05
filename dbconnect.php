38f92e068872bd18a436a479f04450e5491ac000
DanDilag <32535873+DanDilag@users.noreply.github.com>
2018-04-05 10:15:05 +0800

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$VARDB_server = 'localhost';
-$VARDB_user = 'dbadmin';
-$VARDB_pass = 'w3b@dm1n';
-$VARDB_database = 'rtpadb';
-
-$link=mysql_connect("$VARDB_server", "$VARDB_user", "$VARDB_pass");
-mysql_select_db("$VARDB_database");
-?>
